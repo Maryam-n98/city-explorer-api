97c18cf25be55e80948b2a836b3f77bb77de24c6
Maryam-n98 <mariam.mohannad98@gmail.com>
2021-06-02 15:22:29 +0300

    edited

--- server.js
+++ server.js
@@ -1,21 +1,63 @@
-'use strict';
+'use strric';
+
 const express = require('express');
-const server1 = express();
-const cors=require('cors');
 require('dotenv').config();
-const axios = require('axios');
-server1.use(cors());
-const PORT2 =process.env.PORT;
-const weather=require('./weather');
-const movies =require('./movies')
-server1.listen(PORT2,()=>{
-    console.log(`i am listening on port ${PORT2}`);
+const cors = require('cors');
+const server = express();
+const weather =require('./weather.json');
+// const PORT = 3001;
+const PORT = process.env.PORT ;
+server.use(cors());
+
+
+
+server.listen(PORT,() => {
+  
+  console.log( `Listening on PORT ${PORT}`); 
+  
 })
 
-server1.get('/ahmad',weather)
-server1.get('/movies',movies)
 
-server1.get('*',(req,res)=>{
-   
-    res.status(404).send('invalid request to backend')
-});
+      class ForeCast {
+        
+        constructor(object){
+          
+            
+          
+             this.des=`Low of : ${object.low_temp} and a high of ${object.max_temp} with a ${object.weather.description} `
+             this.date= object.valid_date;
+            
+        
+          }
+        }
+        
+server.get('/weather',(req,res)=>{
+  let searchQuery = req.query.cityname;
+
+  let city = weather.find(item=>{
+    if (searchQuery.toLocaleLowerCase() == item.city_name.toLocaleLowerCase() ){
+
+      return item;
+    }
+  });
+  
+      try {
+        
+        let forecasts = city.data.map(item => {
+          
+          return new ForeCast(item);
+        });
+        res.send(forecasts);
+      } 
+      
+      catch {
+        res.status(500).send('OPS!! Your City Not Found');
+      }
+      
+})
+
+
+
+server.get('*', (req, res) => {
+  res.status(404).send(  '"error": "Something went wrong."');
+})
